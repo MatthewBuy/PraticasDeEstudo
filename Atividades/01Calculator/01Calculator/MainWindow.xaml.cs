61edc4779b701d74f29ed61de6c9e276f8063e2c
MatthewBuy <mateusdesouzaamancio@outlook.com>
2020-12-09 16:31:24 -0300

    Adicionando novas atividades

---
+++ Atividades/01Calculator/01Calculator/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01Calculator
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int first = Int32.Parse(txbFirstNumber.Text);
+            int second = Int32.Parse(txbSecondNumber.Text);
+            lblSumResult.Content = first + " + " + second + " = " + (first + second);
+            lblMinResult.Content = first + " - " + second + " = " + (first - second);
+            lblDivResult.Content = first + " / " + second + " = " + (first / second);
+            lblMulResult.Content = first + " * " + second + " = " + (first * second);
+        }
+    }
+}
---
+++ Atividades/TodoApp/TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addToListBox(string item)
+        {
+            lbxTasks.Items.Add(item);
+            txbInput.Text = "";
+            btbAdd.IsEnabled = false;
+        }
+
+        private void txbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(txbInput.Text.Length == 0)
+            {
+                btbAdd.IsEnabled = false;
+            }
+            else
+            {
+                btbAdd.IsEnabled = true;
+                if (e.Key == Key.Enter)
+                {
+                    addToListBox(txbInput.Text);
+
+                }
+            }
+                  
+        }
+
+        private void btbAdd_Click(object sender, RoutedEventArgs e)
+        {
+            addToListBox(txbInput.Text);
+        }
+
+        private void btbDelete_Click(object sender, RoutedEventArgs e)
+        {
+            lbxTasks.Items.Remove(lbxTasks.SelectedItem);
+            btbDelete.IsEnabled = false;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btbDelete.IsEnabled = true;
+        }
+    }
+}
