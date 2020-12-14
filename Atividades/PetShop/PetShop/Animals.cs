3b85d73734490bd80cce5e1bfbf2f53d7c119fd6
MatthewBuy <mateusdesouzaamancio@outlook.com>
2020-12-14 16:44:09 -0300

    Calcula o preço do banho do seu pet.
    Faz o calculo  de quanto que o banho vai sair, baseado  na raça e no tamanho do cachorro.

---
+++ Atividades/PetShop/PetShop/Animals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop
+{
+    class Animals
+    {
+
+        public string DogBreed { get; set; }
+        public string DogSize { get; set; }
+
+     
+
+        
+
+
+        /*public Animals (string dog, string size)
+        {
+            this.DogBreed = dog;
+            this.DogSize = size;
+        }
+
+
+        public void CalculateThePrice(string dog, string size)
+        {
+            decimal price;
+            
+            if(dog == "Beagle" && size == "Medium")
+            {
+                Console.WriteLine($"The price is R${price = 30} reais");
+            }
+            else if (dog == "Beagle" && size == "Small")
+            {
+                Console.WriteLine($"The price is R${price = 15} reais");
+            }else
+            {
+                 Console.WriteLine($"The price is R${price = 50} reais");
+            }
+
+
+        }*/
+
+    }
+}
---
+++ Atividades/PetShop/PetShop/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] raças = { "Beagle", "Bulldog", "Golden Retriever", "Yorkshire Terrier", };
+            string[] tamanhos = { "Grande", "Médio", "Pequeno"};
+            string escolhaR;
+            string escolhaT;
+
+            int valor = 0;
+
+            Console.WriteLine("----------------------------");
+            foreach (var raça in raças)
+            {
+                Console.WriteLine(raça);
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Escreva qual é a raça do seu pet");
+            escolhaR = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("----------------------------");
+            foreach (var tamanho in tamanhos)
+            {
+                Console.WriteLine(tamanho);
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Escreva qual é o tamanho do seu pet");
+            escolhaT = Convert.ToString(Console.ReadLine());
+
+            switch (escolhaR)
+            {
+                case "Beagle":
+                case "Bulldog":
+                case "Yorkshire Terrier":
+                    valor += 15;
+                    break;
+
+                default:
+                    valor += 20;
+                    break;
+            }
+
+            switch (escolhaT)
+            {
+                case"Grande":
+                    valor += 30;
+                    break;
+
+                case "Médio":
+                    valor += 20;
+                    break;
+
+                case "Pequeno":
+                    valor += 10;
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.WriteLine($"O preço para dar banho em seu pet é de: R${valor} reais");
+
+        }
+    }
+}
